45613cfb6d1dd53be2d040b0b8b6000ade1b60b5
Rupam Bhattacharya <rupam.bhattacharya@sap.com>
2023-09-16 15:00:37 +0200

    moving app files to own app folder

---
+++ randomListSelection/cricketPlayingCountries.js
@@ -0,0 +1,19 @@
+cricketPlayingCountries = `Australia
+England
+India
+New Zealand
+Pakistan
+West Indies
+Sri Lanka
+South Africa
+Zimbabwe
+Bangladesh
+Kenya
+Canada
+Ireland
+Scotland
+Netherlands
+United Arab Emirates
+Namibia
+Afghanistan
+Bermuda`
---
+++ randomListSelection/footballPlayingCountries.js
@@ -0,0 +1,30 @@
+footballPlayingCountries = `Argentina
+Germany
+France
+Brazil
+Italy
+Spain
+England
+Portugal
+Netherlands
+Uruguay
+Greece
+Switzerland
+Belgium
+Croatia
+Ecuador
+USA
+Mexico
+Chile
+Ivory Coast
+Japan
+Costa Rica
+Honduras
+Iran
+Nigeria
+Ghana
+Algeria
+Cameroon
+Australia
+South Korea
+Columbia`
